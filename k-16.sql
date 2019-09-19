b0225ac716342c0d2af7c660b0c3d6e9dab9b086
Amy Avirut <amyavirut@gmail.com>
2019-09-19 14:42:58 +0700

    160102

--- k-16.sql
+++ k-16.sql
@@ -1,3 +1,20 @@
 -- 160101
 INSERT INTO types (name)
 VALUES ('Entire Villa'), ('Entire House'), ('Shared House')
+
+-- 160102
+CREATE TABLE users (
+  userid INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR (255),
+  email VARCHAR (255),
+  PRIMARY KEY (userid)
+)
+
+INSERT INTO users (name, email)
+VALUES ('John Smith', 'john@email.com'), ('Sarah Black', 'sarah@gmail.com'), 
+('Albert Philips', 'albert@gmail.com'),('Bob Adams', 'bob@email.com')
+
+
+
+
+
