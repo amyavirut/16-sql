e66eb3cdc87f3cc6bd32a4615527c098c40dea70
Amy Avirut <amyavirut@gmail.com>
2019-09-19 15:07:06 +0700

    160103

--- k-16.sql
+++ k-16.sql
@@ -14,7 +14,22 @@ INSERT INTO users (name, email)
 VALUES ('John Smith', 'john@email.com'), ('Sarah Black', 'sarah@gmail.com'), 
 ('Albert Philips', 'albert@gmail.com'),('Bob Adams', 'bob@email.com')
 
+-- 160103
+CREATE TABLE places (
+  placeid INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR (255),
+  price INT (11),
+  rating INT (11),
+  city VARCHAR(255),
+  type INT (11),
+  host INT (11),
+  PRIMARY KEY (placeid)
+)
 
-
-
-
+INSERT INTO places (title, price, rating, city, type, host)
+VALUES ('Amazing Villa by the Ocean', 150, 5, 'Phuket', 1, 2), 
+('Amazing View in the Jungle', 80, 4, 'Phangan', 2, 1), 
+('Villa with 360 Rooftop', 160, 5, 'Samui',1 , 1), 
+('Cool flat in trendy area', 80, 5, 'Chiang Mai', 2, 4), 
+('Stylish room in city center', 50, 3, 'Bangkok', 3, 2), 
+('Cozy room by the beach', 40, 4, 'Samui', 3, 1)
